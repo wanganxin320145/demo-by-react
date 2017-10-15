cfd9586adfe996d2480d4c1d56470404339d2aa5
wanganxin320145 <58100592@qq.com>
2017-10-15 22:43:42 +0800

    modify Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,3 +1,4 @@
+'use strict';
 require('normalize.css/normalize.css');
 require('styles/App.css');
 
@@ -11,7 +12,6 @@ class AppComponent extends React.Component {
       <div className="index">
         <img src={yeomanImage} alt="Yeoman Generator" />
         <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-        <div>Hellow</div>	
       </div>
     );
   }
